22fce5f8e57b7b670561bf7c285e9e5d9883e3d5
Muhammed Ashir <ashir@webandcrafts.in>
2021-06-15 13:45:23 +0530

    banner designed

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+jQuery( document ).ready(function( $ ) {
+
+	"use strict";
+        
+        if ($('.owl-banner').length) {
+            $('.owl-banner').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 1,
+                margin: 0,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 1,
+                        margin: 20
+                    },
+                    992: {
+                        items: 1,
+                        margin: 30
+                    }
+                }
+            });
+        }
+ 
+});
